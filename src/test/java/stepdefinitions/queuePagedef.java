a3e0736be102f6d24a63aa86c5e14f240e7fab27
SugunaSaran <email4sugu@gmail.com>
2024-12-13 13:06:14 -0500

    changes

---
+++ src/test/java/stepdefinitions/queuePagedef.java
@@ -0,0 +1,189 @@
+package stepdefinitions;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class queuePagedef {
+	@Given("The user is in the Home page after Sign in")
+	public void the_user_is_in_the_home_page_after_sign_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@When("The user clicks the Getting Started button in Queue Panel")
+	public void the_user_clicks_the_getting_started_button_in_queue_panel() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@When("The user selects queue item from the drop down menu")
+	public void the_user_selects_queue_item_from_the_drop_down_menu() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@Then("The user should be directed to Queue Data Structure Page")
+	public void the_user_should_be_directed_to_queue_data_structure_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@Given("The user is in the Queue page after Sign in")
+	public void the_user_is_in_the_queue_page_after_sign_in() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@When("The user clicks Implementation of Queue link in Python page")
+	public void the_user_clicks_implementation_of_queue_link_in_python_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@Then("The user should be redirected to Implementation of Queue in Python page")
+	public void the_user_should_be_redirected_to_implementation_of_queue_in_python_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@Given("The user is on the Implementation of Queue in Python page")
+	public void the_user_is_on_the_implementation_of_queue_in_python_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@When("The user clicks Try Here button in Implementation of Queue in Python page")
+	public void the_user_clicks_try_here_button_in_implementation_of_queue_in_python_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@Then("The user should be redirected to a page having an try Editor with a Run button to test")
+	public void the_user_should_be_redirected_to_a_page_having_an_try_editor_with_a_run_button_to_test() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@Given("The user is in the tryEditor page")
+	public void the_user_is_in_the_try_editor_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@When("The user clicks the Run button without entering the code in the Editor")
+	public void the_user_clicks_the_run_button_without_entering_the_code_in_the_editor() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@Then("The user should not see any error message or output")
+	public void the_user_should_not_see_any_error_message_or_output() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@When("The user clicks the ok button of error alert window")
+	public void the_user_clicks_the_ok_button_of_error_alert_window() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@Then("The user should remain in the tryEditor page with Run button")
+	public void the_user_should_remain_in_the_try_editor_page_with_run_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@When("The user write the valid code in Editor and click the Run button")
+	public void the_user_write_the_valid_code_in_editor_and_click_the_run_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@Then("The user should able to see output in the console")
+	public void the_user_should_able_to_see_output_in_the_console() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@When("The user clicks Implementations using collections.deque link in Python page")
+	public void the_user_clicks_implementations_using_collections_deque_link_in_python_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@Then("The user should be redirected to Implementations using collections.deque page")
+	public void the_user_should_be_redirected_to_implementations_using_collections_deque_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@Given("The user is on the Implementations using collections.deque in Python page")
+	public void the_user_is_on_the_implementations_using_collections_deque_in_python_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@When("The user clicks Try Here button in IImplementations using collections.deque in Python page")
+	public void the_user_clicks_try_here_button_in_i_implementations_using_collections_deque_in_python_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@When("The user clicks Implementations using array link in Python page")
+	public void the_user_clicks_implementations_using_array_link_in_python_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@Given("The user is on the Implementations using array in Python page")
+	public void the_user_is_on_the_implementations_using_array_in_python_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@When("The user clicks Try Here button in Implementations using array in Python page")
+	public void the_user_clicks_try_here_button_in_implementations_using_array_in_python_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@When("The user clicks Queue Operations link in Python page")
+	public void the_user_clicks_queue_operations_link_in_python_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@Then("The user should be redirected to Queue Operations page")
+	public void the_user_should_be_redirected_to_queue_operations_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@Given("The user is on the Queue Operations in Python page")
+	public void the_user_is_on_the_queue_operations_in_python_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@When("The user clicks Try Here button in Queue Operations in Python page")
+	public void the_user_clicks_try_here_button_in_queue_operations_in_python_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@When("The user clicks Practice Questions link in Python page")
+	public void the_user_clicks_practice_questions_link_in_python_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+	@Then("The user should be redirected to Practice Questions page")
+	public void the_user_should_be_redirected_to_practice_questions_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	  
+	}
+
+
+
+}
---
+++ src/test/resources/features/queuePage.feature
@@ -0,0 +1,78 @@
+Feature: Queue
+ 
+
+ 
+  Scenario: Verify that user is able to navigate to queue data structure page
+    Given The user is in the Home page after Sign in
+    When The user clicks the Getting Started button in Queue Panel 
+	  When The user selects queue item from the drop down menu
+    Then The user should be directed to Queue Data Structure Page
+   
+  Scenario: Verify that user is able to navigate to Implementation of Queue in Python page
+    Given The user is in the Queue page after Sign in
+    When The user clicks Implementation of Queue link in Python page
+	  Then The user should be redirected to Implementation of Queue in Python page
+	  
+	 Scenario: Verify that user is able to navigate to try Editor page for Implementation of Queue in Python page
+    Given The user is on the Implementation of Queue in Python page
+    When The user clicks Try Here button in Implementation of Queue in Python page
+	  Then The user should be redirected to a page having an try Editor with a Run button to test
+	 
+	 Scenario: Verify that user receives error when click on Run button without entering code in tryEditor page
+    Given The user is in the tryEditor page
+    When The user clicks the Run button without entering the code in the Editor
+	  Then The user should not see any error message or output
+	  
+	 Scenario: Verify that user receives error for invalid python code in tryEditor page
+    Given The user is in the tryEditor page
+    When The user clicks the Run button without entering the code in the Editor
+	  Then The user should not see any error message or output
+	 
+	 Scenario: Verifying the ok button of error alert message in tryEditor Page
+    Given The user is in the tryEditor page
+    When The user clicks the ok button of error alert window
+	  Then The user should remain in the tryEditor page with Run button
+	 
+	  Scenario: Verify that user is able to see output for valid python code in tryEditor page
+    Given The user is in the tryEditor page
+    When The user write the valid code in Editor and click the Run button
+	  Then The user should able to see output in the console
+	  
+	 Scenario: Verify that user is able to navigate to Implementations using collections.deque page
+    Given The user is in the Queue page after Sign in
+    When The user clicks Implementations using collections.deque link in Python page
+	  Then The user should be redirected to Implementations using collections.deque page
+	  
+	 Scenario:Verify that user is able to navigate to try Editor page for Implementations using collections.deque page
+    Given The user is on the Implementations using collections.deque in Python page
+    When The user clicks Try Here button in IImplementations using collections.deque in Python page
+	  Then The user should be redirected to a page having an try Editor with a Run button to test
+	  
+	 Scenario: Verify that user is able to navigate to Implementations using array page
+    Given The user is in the Queue page after Sign in
+    When The user clicks Implementations using array link in Python page
+	  Then The user should be redirected to Implementations using collections.deque page
+	  
+	 Scenario:Verify that user is able to navigate to try Editor page for Implementations using array page
+    Given The user is on the Implementations using array in Python page
+    When The user clicks Try Here button in Implementations using array in Python page
+	  Then The user should be redirected to a page having an try Editor with a Run button to test
+	  
+	 Scenario: Verify that user is able to navigate to Queue Operations page
+    Given The user is in the Queue page after Sign in
+    When The user clicks Queue Operations link in Python page
+	  Then The user should be redirected to Queue Operations page
+	  
+	 Scenario:Verify that user is able to navigate to try Editor page for Queue Operations page
+    Given The user is on the Queue Operations in Python page
+    When The user clicks Try Here button in Queue Operations in Python page
+	  Then The user should be redirected to a page having an try Editor with a Run button to test
+	  
+	  Scenario: Verify that user is able to navigate to Practice Questions page
+    Given The user is in the Queue page after Sign in
+    When The user clicks Practice Questions link in Python page
+	  Then The user should be redirected to Practice Questions page
+	  
+	
+	  
+	  
