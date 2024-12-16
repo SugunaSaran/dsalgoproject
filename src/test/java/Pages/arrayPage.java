60b8f12f74fe3f03082b6502c569c009b857a2db
Amudhasathish0504 <amudhavarshini@gmail.com>
2024-12-16 13:59:04 -0500

    updated modules

--- src/test/java/Pages/arrayPage.java
+++ src/test/java/Pages/arrayPage.java
@@ -1,7 +1,37 @@
 package Pages;
 
+import org.openqa.selenium.By;
+
 import DriverManager.DriverFactory;
 
 public class arrayPage extends DriverFactory{
+	public arrayPage() {
+		super();
+	}
+	By lnk_ArraysinPython =By.xpath("//a[@href='/array/arrays-in-python/']");
+	By lnk_ArraysUsingList=By.xpath("//a[@href='/array/arrays-using-list/']");
+	By lnk_BasicOperationsinLists=By.xpath("//a[@href='/array/basic-operations-in-lists/']");
+	By lnk_ApplicationsofArray=By.xpath("//a[@href='/array/applications-of-array/']");
+	By lnk_PracticeQuestions=By.xpath("//a[@href='/array/applications-of-array/']");
+	
+	public void navigate_arraysinPythonPage() {
+		
+	}
+    public void navigate_ArraysUsingListPage() {
+		
+	}
+    public void navigate_BasicOperationsinListsPage() {
+		
+	}
+   public void navigate_ApplicationsofArrayPage() {
+		
+	}
+   public void navigate_PracticeQuestionsPage() {
+		
+	}
 
+
+
+
+	
 }
--- src/test/java/Pages/dataStructureIntroductionPage.java
+++ src/test/java/Pages/dataStructureIntroductionPage.java
@@ -1,5 +1,21 @@
 package Pages;
 
-public class dataStructureIntroductionPage {
+import org.openqa.selenium.By;
+
+import DriverManager.DriverFactory;
+
+public class dataStructureIntroductionPage extends DriverFactory  {
+	public dataStructureIntroductionPage() {
+		super();
+	}
+	//locators
+	By lnk_TimeComplexity =By.xpath("//a[@href='time-complexity']");
+	
+	//Action Methods
+    public void navigate_arraysinPythonPage() {
+		
+	 }
+
+	
 
 }
