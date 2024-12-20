f41380355a220e464e14ff4da876e15495073d2e
SugunaSaran <email4sugu@gmail.com>
2024-12-19 19:11:41 -0500

    Updated pom.xml and stackPage,TryEditor page POM

--- src/test/java/testrunners/TestRunner.java
+++ src/test/java/testrunners/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 	    features = "src/test/resources/features",
 	    glue = "stepdefinitions",
-	    tags="@logintag"
+	    tags="@stackPage"
 	)
 
 public class TestRunner extends AbstractTestNGCucumberTests {
