f953087a33cf1f8bed0afc7afe260b27bce2578c
SugunaSaran <email4sugu@gmail.com>
2024-12-21 00:20:23 -0500

    Update TestRunner.java

--- src/test/java/testrunners/TestRunner.java
+++ src/test/java/testrunners/TestRunner.java
@@ -9,15 +9,12 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 	    features = "src/test/resources/features",
 	    glue = {"stepdefinitions","hooks"},
-<<<<<<< HEAD
 	    tags="@stackpage"
 	)
 
 
-=======
-	    
-	    tags="@logintag" )
->>>>>>> 2527d572af3d84453de48d3587b60902031ac224
+
+
 
 public class TestRunner extends AbstractTestNGCucumberTests {
 	
