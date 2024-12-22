87bbebe6509c0a6b3d014ee0b2eeda2104c305de
SugunaSaran <email4sugu@gmail.com>
2024-12-21 23:18:35 -0500

    Updated feature file and step definition file of queue

--- src/test/java/Pages/QueuePage.java
+++ src/test/java/Pages/QueuePage.java
@@ -16,32 +16,6 @@ public class QueuePage extends DriverFactory{
 	By queueOperation=By.xpath("//a[@href='QueueOp']");
 	By practiceQuestion=By.xpath("//a[@href='/queue/practice']");
 	
-	/*public void checkImplementationListLink() {
-		driver.findElement(implementationList).click();
-	}
-	
-	
-	public String validatePageLink() {
-		if(driver.getPageSource().trim().isEmpty())
-			return " ";
-		else
-			return driver.getTitle();
-	}
-	
-	public void checkImplementationCollectionsLink() {
-		driver.findElement(implementationCollections).click();
-	}
-	
-	public void checkImplementationArrayLink() {
-		driver.findElement(implementationarray).click();
-	}
-	public void checkQueueOperationLink() {
-		driver.findElement(queueOperation).click();
-	}
-	public void clickPracticeLink() {
-		checkImplementationCollectionsLink();
-		driver.findElement(practiceQuestion).click();
-	}*/
 	public void checkQueuePageLink(String pageName) {
 		if(pageName.equalsIgnoreCase("implementation-lists"))
 			driver.findElement(implementationList).click();
