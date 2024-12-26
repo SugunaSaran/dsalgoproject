2332005a36ebadf87da3ebccaf1cfa962539f3b3
SugunaSaran <email4sugu@gmail.com>
2024-12-26 11:22:14 -0500

    updated testrunner.java

--- src/test/java/testrunners/TestRunner.java
+++ src/test/java/testrunners/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 	    features = "src/test/resources/features",
 	     glue = {"stepdefinitions","hooks"},
-	     tags="@queuepage"
+	     tags="@queuepage or @stackpage"
  
            )
 	
