8abc3d5ef53fbdbb87042d9b0da280204bc7b4ca
SugunaSaran <email4sugu@gmail.com>
2024-12-27 23:23:23 -0500

    Added extent.properties file

--- src/test/java/testrunners/TestRunner.java
+++ src/test/java/testrunners/TestRunner.java
@@ -1,18 +1,21 @@
 package testrunners;
 import org.testng.annotations.DataProvider;
 
+import com.aventstack.extentreports.ExtentReports;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
 	    features = "src/test/resources/features",
 	     glue = {"stepdefinitions","hooks"},
-	     tags="@graphpage"
+	     tags="@graphpage",
+	     plugin={"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+	     publish=true
  
            )
 	
 
-public class TestRunner extends AbstractTestNGCucumberTests {    
+public class TestRunner extends AbstractTestNGCucumberTests {
 	@Override
     @DataProvider(parallel = true)
     public Object[][] scenarios() {
