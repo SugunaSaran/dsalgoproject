32f367c4570e3c931bbd629674c09889796584a1
SugunaSaran <email4sugu@gmail.com>
2024-12-31 14:44:36 -0500

    Update TestRunner.java
    Updated TestRunner.java to run all the tests

--- src/test/java/testrunners/TestRunner.java
+++ src/test/java/testrunners/TestRunner.java
@@ -10,7 +10,7 @@ import io.cucumber.testng.CucumberOptions;
 
 
 @CucumberOptions(features = "src/test/resources/features", glue = { "stepdefinitions",
-		"hooks" }, tags = "@treepage", plugin = { "pretty",
+		"hooks" }, plugin = { "pretty",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, publish = true)
 
 
